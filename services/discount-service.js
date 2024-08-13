0798582b7ba35c8ebc597c4c7b8587b1ca4a5f5b
Yordi <yordisalvador629@gmail.com>
2024-08-13 15:46:55 -0500

    cambios

---
+++ services/discount-service.js
@@ -0,0 +1,21 @@
+function applyDiscount(product, discountCode) {
+    if (!discountCode) {
+        throw new Error('Discount code is required');
+    }
+
+    if (!discountCode.isValid()) {
+        throw new Error('Invalid discount code');
+    }
+
+    if (discountCode.value > product.price) {
+        throw new Error('Discount value exceeds product price');
+    }
+
+    if (discountCode.value > 0) {
+        product.price -= discountCode.value;
+    }
+
+    return product;
+}
+
+module.exports = { applyDiscount };
